cc7cfad58e7ffa192b5cc0834110fcbd7f8b9751
Matteo Basile <matteo.basile56@gmail.com>
2021-02-02 21:53:51 +0100

    milestone 2 completa, milestone 3 da rivedere

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
 new Vue({
   el: '#app',
    data: {
-contacts: [
-    {
-    name: 'Michele',
-    avatar: '_1',
-    visible: true,
-    messages: [
-          {
-          date: '10/01/2020 15:30:55',
-          text: 'Hai portato a spasso il cane?',
-          status: 'sent'
-          },
-          {
-          date: '10/01/2020 15:50:00',
-          text: 'Ricordati di dargli da mangiare',
-          status: 'sent'
-          },
-          {
-          date: '10/01/2020 16:15:22',
-          text: 'Tutto fatto!',
-          status: 'received'
-          }
-    ],
-},
+     chatText: '',
+     avatarIDX: 0,
+    contacts: [
+        {
+        name: 'Michele',
+        avatar: '_1',
+        visible: true,
+        messages: [
+              {
+              date: '10/01/2020 15:30:55',
+              text: 'Hai portato a spasso il cane?',
+              status: 'sent'
+              },
+              {
+              date: '10/01/2020 15:50:00',
+              text: 'Ricordati di dargli da mangiare',
+              status: 'sent'
+              },
+              {
+              date: '10/01/2020 16:15:22',
+              text: 'Tutto fatto!',
+              status: 'received'
+              }
+        ],
+    },
     {
     name: 'Fabio',
     avatar: '_2',
@@ -85,6 +87,18 @@ contacts: [
         }
     ],
 },
-]}
+]},
+methods:{
+  setActiveContact: function(index) {
+    // setta il contatto attivo
+    this.avatarIDX = index;
+    console.log(this.avatarIDX);
+},
+addToContact: function(element) {
+  this.messages.push(this.chatText);
+console.log(this.chatText);
+}
+}
+
 });
 Vue.config.devtools = true
